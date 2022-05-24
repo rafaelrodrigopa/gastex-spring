f0e9bfe8dcb544f8a6e9c8d70e6af34b802bb409
Rafael Rodrigo <rafael.rodrigo.p.a@gmail.com>
2022-05-23 21:10:18 -0300

    Project created

---
+++ src/main/java/com/rafael/gastexspring/GastexSpringApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.gastexspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastexSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastexSpringApplication.class, args);
+	}
+
+}
