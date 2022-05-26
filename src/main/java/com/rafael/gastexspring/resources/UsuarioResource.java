c4a3f96aaa47e99d23713df4c223b126a404e921
Rafael Rodrigo <rafael.rodrigo.p.a@gmail.com>
2022-05-26 20:17:26 -0300

    Service layer, component registration

--- src/main/java/com/rafael/gastexspring/resources/UsuarioResource.java
+++ src/main/java/com/rafael/gastexspring/resources/UsuarioResource.java
@@ -1,20 +1,35 @@
 package com.rafael.gastexspring.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rafael.gastexspring.entities.Usuario;
+import com.rafael.gastexspring.services.UsuarioService;
 
 @RestController
 @RequestMapping(value = "/Usuarios")
 public class UsuarioResource {
 
+	@Autowired
+	private UsuarioService service;
+	
 	@GetMapping
-	public ResponseEntity<Usuario> findAll(){
+	public ResponseEntity<List<Usuario>> findAll(){
 		
-		Usuario u = new Usuario(1L, "Rafael", "rafael@gmail.com");
-		return ResponseEntity.ok().body(u);
+		List<Usuario> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Usuario> findById(@PathVariable Long id){
+		Usuario obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
---
+++ src/main/java/com/rafael/gastexspring/services/UsuarioService.java
@@ -0,0 +1,27 @@
+package com.rafael.gastexspring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rafael.gastexspring.entities.Usuario;
+import com.rafael.gastexspring.repositories.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository repository;
+	
+	public List<Usuario> findAll(){
+		return repository.findAll();
+	}
+	
+	public Usuario findById(Long id) {
+		Optional<Usuario> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
