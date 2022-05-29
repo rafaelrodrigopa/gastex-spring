05c7f4ef8f8bbd72bcfa1f123581cdd6d0c9134b
Rafael Rodrigo <rafael.rodrigo.p.a@gmail.com>
2022-05-29 01:21:14 -0300

    Delete Categoria

--- src/main/java/com/rafael/gastexspring/services/CategoriaService.java
+++ src/main/java/com/rafael/gastexspring/services/CategoriaService.java
@@ -27,4 +27,8 @@ public class CategoriaService {
 	public Categoria insert(Categoria obj) {
 		return repository.save(obj);
 	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
 }
