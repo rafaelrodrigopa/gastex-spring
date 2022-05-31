e330f9864c032808196fc503e826943b5c4f991d
Rafael Rodrigo <rafael.rodrigo.p.a@gmail.com>
2022-05-30 22:31:49 -0300

    Exception handling - delete

--- src/main/java/com/rafael/gastexspring/services/UsuarioService.java
+++ src/main/java/com/rafael/gastexspring/services/UsuarioService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.rafael.gastexspring.entities.Usuario;
 import com.rafael.gastexspring.repositories.UsuarioRepository;
+import com.rafael.gastexspring.services.exceptions.DatabaseException;
 import com.rafael.gastexspring.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UsuarioService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public Usuario update(Long id, Usuario obj) {
